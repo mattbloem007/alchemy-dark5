5215b7f7ec0cf2c534dbe42965ed72880ec0fe12
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2023-06-21 13:34:59 +0200

    testing omnisend connect

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -20,7 +20,7 @@ import React from 'react'
 
 export function onRenderBody(
   { setHeadComponents,
-    //setPreBodyComponents,
+    setPreBodyComponents,
   }
 ) {
  setHeadComponents([
@@ -28,6 +28,16 @@ export function onRenderBody(
     type="text/javascript"
     src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=VECYLf"
     ></script>,
+    <script
+     dangerouslySetInnerHTML={{
+        __html:`
+        window.omnisend = window.omnisend || [];
+        omnisend.push(["accountID", "64917181025b19a15626b18b"]);
+        omnisend.push(["track", "$pageViewed"]);
+        !function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://omnisnippet1.com/inshop/launcher-v2.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
+   	 `
+     }}
+     />
   ])
   // setPreBodyComponents([
   //   <div id="checkout">
